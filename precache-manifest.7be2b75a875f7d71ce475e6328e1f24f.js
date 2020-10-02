472e6e19dfcba58ee9cc5d28bc1ffd5cdfbc1960
Aalya <aalya.banu@gmail.com>
2020-10-02 21:40:06 +0100

    Updates

---
+++ precache-manifest.7be2b75a875f7d71ce475e6328e1f24f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a584c05b708c88b8bfc3c1fd300204",
+    "url": "/EventsAPPclient/index.html"
+  },
+  {
+    "revision": "2d7c0a8c949765d42aa9",
+    "url": "/EventsAPPclient/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5afa8b8db5f6c9078d67",
+    "url": "/EventsAPPclient/static/css/main.10aa0a36.chunk.css"
+  },
+  {
+    "revision": "2d7c0a8c949765d42aa9",
+    "url": "/EventsAPPclient/static/js/2.7b5edd75.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/EventsAPPclient/static/js/2.7b5edd75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afa8b8db5f6c9078d67",
+    "url": "/EventsAPPclient/static/js/main.34265ffa.chunk.js"
+  },
+  {
+    "revision": "39957540ddf9101dc229",
+    "url": "/EventsAPPclient/static/js/runtime-main.5050b8ab.js"
+  }
+]);
